1dbf10af0780f4107ba9eac4e44e1f5f853e701d
suzukiryoji <suzukiryoji@localhost>
2021-03-31 00:32:27 +0900

    write codes

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("メソッドを呼び出します");
+		hello();
+		System.out.println("メソッドの呼び出しが終わりました");
+		methodA();
+	}
+	public static void hello() {
+		System.out.println("湊さん、こんにちは");
+	}
+
+	public static void methodA() {
+		System.out.println("methodA");
+		methodB();
+	}
+	public static void methodB() {
+		System.out.println("methodB");
+	}
+
+}
+
