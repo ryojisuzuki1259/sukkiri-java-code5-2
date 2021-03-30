316ba9f44a69ce0d23e3f9b5d0706a3b52ce12e7
suzukiryoji <suzukiryoji@localhost>
2021-03-31 00:36:21 +0900

    Rewrite codes

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,14 @@
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("メソッドを呼び出します");
-		hello();
+		hello("湊");
+		hello("朝香");
+		hello("菅原");
 		System.out.println("メソッドの呼び出しが終わりました");
-		methodA();
-	}
-	public static void hello() {
-		System.out.println("湊さん、こんにちは");
-	}
 
-	public static void methodA() {
-		System.out.println("methodA");
-		methodB();
 	}
-	public static void methodB() {
-		System.out.println("methodB");
+	public static void hello(String name) {
+		System.out.println(name + "さん、こんにちは");
 	}
 
 }
