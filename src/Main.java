ff6b7067cf65e860076dc24e4dcab22407ed6e09
suzukiryoji <suzukiryoji@localhost>
2021-04-01 01:04:47 +0900

    Rewrite codes

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,22 @@
 
 public class Main {
+	// １つ目のメソッド
 	public static int add(int x, int y) {
-		int ans = x + y;
-		return ans;
+		return x + y;
+	}
+	// ２つ目のメソッド
+	public static double add(double x, double y) {
+		return x + y;
+	}
+	// ３つ目のメソッド
+	public static String add(String x, String y) {
+		return x + y;
 	}
 
 	public static void main(String[] args) {
-		System.out.println(add(add(10, 20), add(30, 40)));
+		System.out.println(add(10, 20));
+		System.out.println(add(3.5, 2.7));
+		System.out.println(add("Hello", "World"));
 	}
 }
 
